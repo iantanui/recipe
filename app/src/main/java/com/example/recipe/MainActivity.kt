22c4a9ae1662f5c0b8c2dbaea506f57decb6b1d9
iantanui <rutooian@gmail.com>
2024-03-01 11:16:24 +0300

    recipe model

--- app/src/main/java/com/example/recipe/MainActivity.kt
+++ app/src/main/java/com/example/recipe/MainActivity.kt
@@ -3,13 +3,24 @@ package com.example.recipe
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.recipe.ui.theme.RecipeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    RecipeApp()
                 }
             }
         }
@@ -30,17 +41,80 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun RecipeApp() {
+    val navController = rememberNavController()
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+
+        NavHost(navController = navController, startDestination = "recipeList") {
+            composable("recipeList") {
+                RecipeListScreen(navController = navController)
+            }
+            composable(
+                "recipeDetail/{recipeName}",
+                arguments = listOf(navArgument("recipeName") { type = NavType.StringType })
+            ) { backStackEntry ->
+                val recipeName = backStackEntry.arguments?.getString("recipeName")
+                val recipe = recipes.find { it.name == recipeName }
+                if (recipe != null) {
+                    RecipeDetailScreen(recipe = recipe)
+                } else {
+                    Text(text = "Recipe not found")
+                }
+            }
+        }
+    }
+}
+
+data class Recipe(
+    val name: String,
+    val ingredients: List<String>,
+    val instructions: String,
+)
+
+val recipes = listOf(
+    Recipe("Pasta", listOf("Pasta", "Tomato Sauce"), "Cook pasta and mix with tomato sauce.")
+)
+
+@Composable
+fun RecipeListScreen(navController: NavHostController) {
+    Column {
+        for (recipe in recipes) {
+            RecipeListItem(
+                recipe = recipe,
+                onClick = { navController.navigate("recipeDetail/${recipe.name}") })
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+    }
+}
+
+@Composable
+fun RecipeListItem(recipe: Recipe, onClick: () -> Unit) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = recipe.name,
+        style = MaterialTheme.typography.bodyMedium,
+        modifier = Modifier
+            .clickable(onClick = onClick)
     )
+}
+
+@Composable
+fun RecipeDetailScreen(recipe: Recipe) {
+    Column {
+        Text(text = recipe.name, style = MaterialTheme.typography.bodyMedium)
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(text = "Ingredients: ${recipe.ingredients.joinToString(",")}")
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(text = "Instructions: ${recipe.instructions}")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     RecipeTheme {
-        Greeting("Android")
+        RecipeApp()
     }
 }
