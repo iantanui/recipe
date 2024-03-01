1260d464592771ea4954bdd7cf5847c7d1f1c196
iantanui <rutooian@gmail.com>
2024-03-01 13:25:10 +0300

    add topAppBar

--- app/src/main/java/com/example/recipe/MainActivity.kt
+++ app/src/main/java/com/example/recipe/MainActivity.kt
@@ -1,19 +1,34 @@
 package com.example.recipe
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -81,36 +96,99 @@ val recipes = listOf(
     Recipe("Salad", listOf("Lettuce", "Tomato", "Cucumber"), "Chop vegetables and mix in a bowl.")
 )
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RecipeListScreen(navController: NavHostController) {
-    Column {
-        for (recipe in recipes) {
-            RecipeListItem(
-                recipe = recipe,
-                onClick = { navController.navigate("recipeDetail/${recipe.name}") })
-            Spacer(modifier = Modifier.height(16.dp))
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "Recipes",
+                        textAlign = TextAlign.Center,
+                        style = MaterialTheme.typography.headlineLarge
+                    )
+                }
+            )
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(top = 56.dp)
+        ) {
+            LazyColumn(
+                modifier = Modifier
+                    .padding(10.dp)
+            ) {
+                items(recipes) { recipe ->
+                    RecipeListItem(
+                        recipe = recipe,
+                        onClick = { navController.navigate("recipeDetail/${recipe.name}") })
+                    Spacer(modifier = Modifier.height(16.dp))
+                }
+            }
         }
     }
 }
 
 @Composable
 fun RecipeListItem(recipe: Recipe, onClick: () -> Unit) {
-    Text(
-        text = recipe.name,
-        style = MaterialTheme.typography.bodyMedium,
+    Card(
         modifier = Modifier
+            .fillMaxWidth()
             .clickable(onClick = onClick)
-    )
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+            Text(
+                text = recipe.name,
+                style = MaterialTheme.typography.headlineMedium
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = "Ingredients: ${recipe.ingredients.joinToString(", ")}")
+        }
+    }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RecipeDetailScreen(recipe: Recipe) {
-    Column {
-        Text(text = recipe.name, style = MaterialTheme.typography.bodyMedium)
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(text = "Ingredients: ${recipe.ingredients.joinToString(",")}")
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(text = "Instructions: ${recipe.instructions}")
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = recipe.name) },
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Back"
+                        )
+                    }
+                }
+            )
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start
+        ) {
+            Text(text = "Ingredients")
+            Text(
+                text = recipe.ingredients.joinToString(","),
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = "Instructions", style = MaterialTheme.typography.bodyMedium)
+            Text(text = recipe.instructions)
+        }
+
     }
 }
 
